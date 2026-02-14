8d22b1408864fd07aaf661977264e2f75bb9ceda
suyashsahu00 <suyashsahu00@gmail.com>
2026-02-14 19:29:28 +0530

    feat: Add initial project setups for function parameters and truthy/falsy values lessons, including HTML, CSS, JavaScript, and Vite configurations.

---
+++ 24-12-2025/Build a Chrome Extension/Truthy and falsy values/index.js
@@ -0,0 +1,20 @@
+// const credits = 0
+
+// if (credits) {
+//     console.log("Let's play 🎰")
+// } else {
+//     console.log("Sorry, you have no credits 😭")
+// }
+
+// truthy
+// falsy
+
+// false
+// 0
+// ""
+// null -> how you as a developer signalize emptiness
+// undefined -> how JavaScript signalizes emptiness 
+
+let currentViewers = ["jane"]
+
+console.log(currentViewers[5])
