a7506ffd3d7e5aca7ec8188cfb10ba81f6cddf4a
suyashsahu00 <suyashsahu00@gmail.com>
2026-02-16 20:12:30 +0530

    feat: Add array parameter example with webpack and HTML configuration.

---
+++ 24-12-2025/Build a Chrome Extension/Arrays as parameters/index.js
@@ -0,0 +1,11 @@
+// Create a function, getFirst(arr), that returns the first item in the array
+
+function getFirst(arr) {
+    return arr[0]
+}
+
+let firstCard = getFirst([10, 2, 5])
+
+console.log(firstCard)
+
+// Call it with an array as an argument to verify that it works
