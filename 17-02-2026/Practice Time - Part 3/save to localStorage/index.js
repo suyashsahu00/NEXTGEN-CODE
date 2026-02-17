564cda4b9ca085845da8e026200a5b46fba85d4f
suyashsahu00 <suyashsahu00@gmail.com>
2026-02-17 20:14:33 +0530

    feat: Initialize a new Vite project demonstrating basic `localStorage` functionality.

---
+++ 17-02-2026/Practice Time - Part 3/save to localStorage/index.js
@@ -0,0 +1,8 @@
+// Save a value to localStorage
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
+
+localStorage.setItem("myCredit",100)
+
+let myCredit = localStorage.getItem("myCredit")
+console.log(myCredit)
