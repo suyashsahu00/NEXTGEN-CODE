2fa55add66bd830f4443660ccc748eed261c2d37
suyashsahu00 <suyashsahu00@gmail.com>
2026-04-10 19:29:47 +0530

    feat: add C++ example demonstrating class constructor implementation

--- 02DSA/cpp_code/constructor.cpp
+++ 02DSA/cpp_code/constructor.cpp
@@ -1,20 +1,28 @@
-// A constructor is a special type of member function that is called automatically when an object is created
-// A constructor has the same name as that of the class.
-// A constructor does not have a return type.
+// A constructor is a special type of member function that is called
+// automatically when an object is created A constructor has the same name as
+// that of the class. A constructor does not have a return type.
 #include <iostream>
 #include <string>
 using namespace std;
 
-class Myclass {
+class Car {
 public:
-  Myclass(int x, double y, string z) ;
+  string brand;
+  string model;
+  int year;
+  Car(string x, string y, int z);
 };
-Myclass::Myclass(int x,double y,string z) {
-  cout << x << endl;
-  cout << y << endl;
-  cout << z;
+Car::Car(string x, string y, int z) {
+  brand = x;
+  model = y;
+  year = z;
 }
 int main() {
-  Myclass myobj1(06,19.89,"This is text of song");
+  Car Obj1("BMW", "X5", 1999);
+  Car obj2("Ford", "Mustang", 1969);
+
+  cout << Obj1.brand << " " << Obj1.model << " " << Obj1.year << endl;
+  cout << obj2.brand << " " << obj2.model << " " << obj2.year << endl;
+  
   return 0;
 }
