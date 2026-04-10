b2fc7411ab850a2888ed0ca44d1059b3e1707b2f
suyashsahu00 <suyashsahu00@gmail.com>
2026-04-10 18:36:04 +0530

    feat: add C++ implementation of Car class with basic object instantiation and member access

---
+++ 02DSA/cpp_code/oops_cpp1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Car {
+public:
+  string brand;
+  string model;
+  int year;
+  int speed(int maxSpeed);
+};
+
+int Car::speed(int maxSpeed) {
+  return maxSpeed;
+}
+
+int main() {
+  Car carobj1;
+  carobj1.brand = "BMW";
+  carobj1.model = "X5";
+  carobj1.year = 1999;
+  Car carobj2;
+  carobj2.brand = "Ford";
+  carobj2.model = "Mustang";
+  carobj2.year = 1969;
+
+  cout << carobj1.brand << " " << carobj1.model << " " << carobj1.year << endl;
+  cout << carobj2.brand << " " << carobj2.model << " " << carobj2.year << endl;
+  
+  return 0;
+}
