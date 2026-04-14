2fc41f1ca7db382a29198a6c65db379ec65dc9e9
suyashsahu00 <suyashsahu00@gmail.com>
2026-04-14 20:53:29 +0530

    feat: add C++ example demonstrating public, private, and protected access modifiers

--- 02DSA/cpp_code/access_modifiers.cpp
+++ 02DSA/cpp_code/access_modifiers.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 using namespace std;
 
-class Employee{
-
-    int salary=10000; //declared private here
-    public:
-        string name;
-        string post;
+class Employee {
+    int salary = 10000; //declared private here
+public:
+    string name;
+protected:
+    int id = 101; // protected: sirf child class me dikhega
 };
 
-int main(){
+class Child : public Employee {
+public:
+    void show() { cout << "ID is: " << id; } // accessible here
+};
+
+int main() {
     Employee e1;
-    cout<<"Enter your name: ";
-    cin>>e1.name;
-    cout<<"Your name is: "<<e1.name<<endl;
-    cout<<"Enter your post: ";
-    cin>>e1.post;
-    cout<<"Your post is: "<<e1.post;
-    // cout<<e1.salary;//'int Employee::salary' is private within this context
+    cout << "Enter name: ";
+    cin >> e1.name;
+    cout << "Your name is: "<< e1.name << endl;
+
+    // cout << e1.salary; //'int Employee::salary' is private within this context
+    // cout << e1.id;     // Error: protected main me nahi chalta
+
+    Child c1;
+    c1.show(); // child class ke through access
+
     return 0;
 }
