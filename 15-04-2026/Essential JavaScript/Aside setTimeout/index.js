555d49d6087247d9a623b2a39286fb9deeb9ebb1
suyashsahu00 <suyashsahu00@gmail.com>
2026-04-15 22:56:19 +0530

    feat: initialize new project for setTimeout practice exercise

---
+++ 15-04-2026/Essential JavaScript/Aside setTimeout/index.js
@@ -0,0 +1,17 @@
+console.log('What is the capital of Peru?')
+
+setTimeout(function(){
+    console.log('Lima!')
+}, 3000)
+
+setTimeout(function(){
+    console.log('Ready for next question?')
+}, 6000)
+
+
+
+/*
+Challenge:
+1. Make the third console.log fire 3 seconds after 
+   the answer appears.
+*/
