329cfa32a55d9237f37f8e6e71f0e6317c55f85b
suyashsahu00 <suyashsahu00@gmail.com>
2026-06-13 16:32:23 +0530

    feat: implement preventDefault challenge for form submission with modal interaction

---
+++ 15-04-2026/Essential JavaScript/preventDefault Challenge/index.js
@@ -0,0 +1,28 @@
+const modal = document.getElementById('modal')
+const modalCloseBtn = document.getElementById('modal-close-btn')
+
+setTimeout(function(){
+    modal.style.display = 'inline'
+}, 1500)
+
+modalCloseBtn.addEventListener('click', function(){
+    modal.style.display = 'none'
+})
+
+const loginForm = document.getElementById('consent-form')
+
+loginForm.addEventListener('submit',function(e){
+    e.preventDefault()
+    console.log('form submitted')
+})
+
+/*
+Challenge:
+1. Take control of the form element. 
+2. Add an eventListener to the form to listen for a
+   "submit" event.
+3. When a user clicks "accept", prevent the default
+   behaviour that triggers the refresh.
+4. Log out "form submitted".
+*/  
+
