95ab60dd8b58b8f187f7bcf2a2b60b8558e7b8ea
suyashsahu00 <suyashsahu00@gmail.com>
2026-09-06 11:23:33 +0530

    feat: initialize Vite-powered leads tracker application with Firebase integration

---
+++ 04-09-2026/Learn JavaScript/Build a Mobile App/Delete unneeded code/index.js
@@ -0,0 +1,37 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
+
+const firebaseConfig = {
+    databaseURL: process.env.DATABASE_URL
+}
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    
+})
+
+inputBtn.addEventListener("click", function() {
+    console.log(inputEl.value)
+    inputEl.value = ""
+})
